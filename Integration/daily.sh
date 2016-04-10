61690536270c4e4ee40be87ba2d2583f4264f9e3
Pat Smuk <patrick.smuk@uoit.net>
2016-04-10 15:19:04 -0400

    Update daily.sh to match change from .TF to .tf.txt

--- Integration/daily.sh
+++ Integration/daily.sh
@@ -22,8 +22,8 @@ if [ "$#" -ne 1 ]; then
   exit
 fi
 if [ $1 -eq 1 ]; then
-  echo "00000 END_OF_FILE          D 00000.00 N" > Accounts/current-accounts.txt 
-  echo "00000 END_OF_FILE          D 00000.00 0000 N" > Accounts/master-accounts.txt 
+  echo "00000 END_OF_FILE          D 00000.00 N" > Accounts/current-accounts.txt
+  echo "00000 END_OF_FILE          D 00000.00 0000 N" > Accounts/master-accounts.txt
 
 fi
 echo "Day $1"
@@ -35,19 +35,19 @@ currentAccountsFile="Accounts/current-accounts.txt"
 masterAccountsFile="Accounts/master-accounts.txt"
 frontEnd="../LSR/bin/frontend/frontend"
 backEnd="Default.Main"
-mergedFile="merged-transactions.TF"
+mergedFile="merged-transactions.tf.txt"
 # Remove all files in the transaction directory
-rm $(pwd)/$transactionDirectory/day$day/*.TF
+rm $(pwd)/$transactionDirectory/day$day/*.tf.txt
 # Read in the commands for the day and start the front end with redirected input
 for commands in $(pwd)/$commandsDirectory/day$day/*.txt
 do
   filename=$(basename $commands)
   echo "Processing $filename"
   #run the front end
-  $frontEnd $(pwd)/$currentAccountsFile $(pwd)/$transactionDirectory/day$day/${filename%.txt}.TF < $(pwd)/$commandsDirectory/day$day/$filename
+  $frontEnd $(pwd)/$currentAccountsFile $(pwd)/$transactionDirectory/day$day/${filename%.txt}.tf.txt < $(pwd)/$commandsDirectory/day$day/$filename
 done
 # Merge the transaction files
-for transactions in $(pwd)/$transactionDirectory/day$day/*.TF
+for transactions in $(pwd)/$transactionDirectory/day$day/*.tf.txt
 do
   cat $transactions >> $(pwd)/$transactionDirectory/day$day/$mergedFile
 done
